c82133f487279b646f91078176905433124f9e4f
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-05 13:34:55 +0430

    Add DbContextFactor for design-time migrations and operations

---
+++ MateMachine.CurrencyConverter.Data/DbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MateMachine.CurrencyConverter.Data {
+    internal class DbContextFactory : IDesignTimeDbContextFactory<CurrencyConverterDbContext> {
+        public CurrencyConverterDbContext CreateDbContext(string[] args) {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var optionsBuilder = new DbContextOptionsBuilder<CurrencyConverterDbContext>();
+            optionsBuilder.UseSqlServer(/*args[0]*/ config.GetConnectionString("DbConnex"));
+            return new CurrencyConverterDbContext(optionsBuilder.Options);
+        }
+    }
+}
