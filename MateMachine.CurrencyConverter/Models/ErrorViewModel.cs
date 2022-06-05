b1a2c0f5f5b85ed80c8c17d06c6f32752370cc6c
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-05 12:09:21 +0430

    Add project files.

---
+++ MateMachine.CurrencyConverter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MateMachine.CurrencyConverter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
