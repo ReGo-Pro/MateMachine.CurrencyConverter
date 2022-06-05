a81d32946b43716137d114a69525816bb8a4e3df
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-05 12:33:11 +0430

    Add CurrenyExchangeRate entity class

---
+++ MateMachine.CurrencyConverter.Data/Entities/CurrenyExchangeRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Data.Entities {
+    public class CurrenyExchangeRate {
+        public int Id { get; set; }
+        public Currency FromCurrency { get; set; }
+        public Currency ToCurrency { get; set; }
+        public double ExchangeRate { get; set; }
+    }
+}
