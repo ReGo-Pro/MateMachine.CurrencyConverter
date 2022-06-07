d6713234f0e87bfef6e832257b39ec7c3fd65cb4
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-08 02:38:51 +0430

    Add CurrencyExchangeRateRepository to project

---
+++ MateMachine.CurrencyConverter.Data/Interfaces/ICurrencyExchangeRateRepository.cs
@@ -0,0 +1,7 @@
+﻿using MateMachine.CurrencyConverter.Data.Entities;
+
+namespace MateMachine.CurrencyConverter.Data.Interfaces {
+    internal interface ICurrencyExchangeRateRepository : IRepository<CurrenyExchangeRate> {
+        CurrenyExchangeRate GetExchangeRate(Currency from, Currency to);
+    }
+}
---
+++ MateMachine.CurrencyConverter.Data/Repositories/CurrencyExchangeRateRepository.cs
@@ -0,0 +1,23 @@
+﻿using MateMachine.CurrencyConverter.Data.Entities;
+using MateMachine.CurrencyConverter.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Data.Repositories {
+    public class CurrencyExchangeRateRepository : GenericRepository<CurrenyExchangeRate>, ICurrencyExchangeRateRepository {
+        public CurrencyExchangeRateRepository(CurrencyConverterDbContext dbContext) : base(dbContext) {
+        }
+
+        public CurrenyExchangeRate GetExchangeRate(Currency from, Currency to) {
+            return DbContext.ExchangeRates.FirstOrDefault(e => e.FromCurrency == from && e.ToCurrency == to);
+        }
+
+        private CurrencyConverterDbContext DbContext {
+            get => dbContext as CurrencyConverterDbContext;
+        }
+    }
+}
