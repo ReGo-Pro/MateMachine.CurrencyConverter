399db2fa79a475b3cc7f028f12e8e295870ff527
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-08 02:29:27 +0430

    Add CurrencyRepository injection to CurrencyConverter class

--- MateMachine.CurrencyConverter.Business/CurrencyConverter.cs
+++ MateMachine.CurrencyConverter.Business/CurrencyConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MateMachine.CurrencyConverter.Data.Interfaces;
+using MateMachine.CurrencyConverter.Data.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,22 @@ using System.Threading.Tasks;
 
 namespace MateMachine.CurrencyConverter.Business {
     public class CurrencyConverter : ICurrencyConverter {
+        private ICurrencyRepository _currencyRepository;
+
+        public CurrencyConverter(ICurrencyRepository currencyRepository) {
+            _currencyRepository = currencyRepository;
+        }
+
         public void ClearConfiguration() {
             throw new NotImplementedException();
         }
 
         public double Convert(string fromCurrency, string toCurrency, double amount) {
+            var source = _currencyRepository.GetByName(fromCurrency);
+            var destination = _currencyRepository.GetByName(toCurrency);
+
+            // We need the exchange rate repository here
+
             throw new NotImplementedException();
         }
 
