5c8cb36e782636e22e007c2e8f90563a994f90db
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-08 02:00:03 +0430

    Add CurrencyConverter class and interface to the project

---
+++ MateMachine.CurrencyConverter.Business/CurrencyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Business {
+    public class CurrencyConverter : ICurrencyConverter {
+        public void ClearConfiguration() {
+            throw new NotImplementedException();
+        }
+
+        public double Convert(string fromCurrency, string toCurrency, double amount) {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates) {
+            throw new NotImplementedException();
+        }
+    }
+}
