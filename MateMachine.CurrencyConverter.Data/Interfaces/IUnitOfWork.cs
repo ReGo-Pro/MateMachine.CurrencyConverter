3102b334225d4815cc0dd5faccebeea749e54d5b
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-08 11:02:05 +0430

    Add unit of work to data project

---
+++ MateMachine.CurrencyConverter.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Data.Interfaces {
+    public interface IUnitOfWork {
+        ICurrencyRepository CurrencyRepo { get; }
+        ICurrencyExchangeRateRepository ExchangeRateRepo { get; }
+        void CompleteAsync();
+    }
+}
---
+++ MateMachine.CurrencyConverter.Data/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using MateMachine.CurrencyConverter.Data.Interfaces;
+using MateMachine.CurrencyConverter.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Data {
+    public class UnitOfWork : IUnitOfWork {
+        private CurrencyConverterDbContext _dbContext;
+        public UnitOfWork(CurrencyConverterDbContext dbContext) {
+            _dbContext = dbContext;
+            CurrencyRepo = new CurrencyRepository(dbContext);
+            ExchangeRateRepo = new CurrencyExchangeRateRepository(dbContext);
+        }
+
+        public ICurrencyRepository CurrencyRepo { get; private set;}
+        public ICurrencyExchangeRateRepository ExchangeRateRepo { get; private set; }
+
+        public void CompleteAsync() {
+            _dbContext.SaveChangesAsync();
+        }
+    }
+}
