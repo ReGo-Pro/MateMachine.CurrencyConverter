ee4673e2440e2dea7cc94286d4e62c4ce6d2ee62
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-09 20:05:41 +0430

    Unit testing convert class with 100 requests

---
+++ MateMachine.CurrencyConverter.Tests/When_Invoked_By_100_Threads.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Tests {
+    [TestClass]
+    public class When_Invoked_By_100_Threads : ConverterTestBase {
+        double?[] resultArray = new double?[100];
+
+        public async Task Act() {
+            for (int i = 0; i < 100; i++) {
+                resultArray[i] = await Task.Run(() => CurrencyConverter.ConvertAsync(Currencies.Single(c => c.Name == "USD"), Currencies.Single(c => c.Name == "GBP"), InitialAmount));
+            }
+        }
+
+        [TestMethod]
+        public async Task Then_Results_Are_Valid() {
+            await Act();
+            for (int i = 0; i < 100; i++) {
+                Assert.AreEqual(7.772, Math.Round(resultArray[i].Value, 5));
+            }
+        }
+    }
+}
