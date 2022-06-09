c34dc5bcf99a0569034359eaf57ef00f8aa37c05
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-09 20:25:55 +0430

    Cache calculated routes for quicker lookup when requested again

--- MateMachine.CurrencyConverter.Business/CurrencyConverter.cs
+++ MateMachine.CurrencyConverter.Business/CurrencyConverter.cs
@@ -6,16 +6,20 @@ namespace MateMachine.CurrencyConverter.Business {
     public class CurrencyConverter : ICurrencyConverter {
         private ConcurrentBag<Currency> _allCurrencies;
         private ConcurrentBag<CurrencyExchangeRate> _allExchangeRates;
+        private ConcurrentDictionary<(Currency, Currency), List<Currency>> _routeCache;
+
         public bool IsInitialzied { get; private set; }
 
         public CurrencyConverter() {
             _allCurrencies = new ConcurrentBag<Currency>();
             _allExchangeRates = new ConcurrentBag<CurrencyExchangeRate>();
+            _routeCache = new ConcurrentDictionary<(Currency,Currency), List<Currency>>();
         }
 
         public void ClearConfiguration() {
             _allCurrencies.Clear();
             _allExchangeRates.Clear();
+            _routeCache.Clear();
         }
 
         // This method is still unsafe
@@ -24,10 +28,17 @@ namespace MateMachine.CurrencyConverter.Business {
             if (directExchangeRate == null) {
                 var reverseExchangeRate = _allExchangeRates.SingleOrDefault(c => c.FromCurrencyId == toCurrency.Id && c.ToCurrencyId == fromCurrency.Id);
                 if (reverseExchangeRate == null) {
-                    CurrencyConversionGraph currencyConversionGraph = new CurrencyConversionGraph();
-                    currencyConversionGraph.AddNodes(_allCurrencies);
-                    currencyConversionGraph.AddEdges(_allExchangeRates);
-                    var shortestPath = currencyConversionGraph.GetShortestPath(fromCurrency, toCurrency);
+                    List<Currency> shortestPath = null;
+                    if (_routeCache.ContainsKey((fromCurrency, toCurrency))) {
+                        shortestPath = _routeCache[(fromCurrency, toCurrency)];
+                    }
+                    else {
+                        CurrencyConversionGraph currencyConversionGraph = new CurrencyConversionGraph();
+                        currencyConversionGraph.AddNodes(_allCurrencies);
+                        currencyConversionGraph.AddEdges(_allExchangeRates);
+                        shortestPath = currencyConversionGraph.GetShortestPath(fromCurrency, toCurrency);
+                        _routeCache[(fromCurrency, toCurrency)] = shortestPath;
+                    }
                     if (shortestPath == null) {
                         return null;
                     }
@@ -57,12 +68,17 @@ namespace MateMachine.CurrencyConverter.Business {
 
         public void UpdateConfiguration(IEnumerable<(Currency FromCurrency, Currency ToCurrency, double ExchangeRate)> conversionRates) {
             IsInitialzied = true;
+
             foreach (var conversionRate in conversionRates) {
                 if (!_allCurrencies.Contains(conversionRate.FromCurrency)) {
                     _allCurrencies.Add(conversionRate.FromCurrency);
+                    _routeCache.Clear();
+                    // Adding a new node renders cache invalid
                 }
                 if (!_allCurrencies.Contains((Currency)conversionRate.ToCurrency)) {
                     _allCurrencies.Add(conversionRate.ToCurrency);
+                    _routeCache.Clear();
+                    // Adding a new node renders cache invalid
                 }
                 var existingRate = _allExchangeRates.FirstOrDefault(er => er.FromCurrencyId == conversionRate.FromCurrency.Id && er.ToCurrencyId == conversionRate.ToCurrency.Id);
                 if (existingRate == null) {
@@ -73,8 +89,11 @@ namespace MateMachine.CurrencyConverter.Business {
                         ToCurrencyId = conversionRate.ToCurrency.Id,
                         ExchangeRate = conversionRate.ExchangeRate
                     });
+                    _routeCache.Clear();    // Adding new edge renders this cache invalid
                 }
                 else {
+                    // I do not cache exchange rates themselves because they tend to change frequently and updating the cache every time creates extra overhead.
+                    // But it is less likely to add new node (currency) or edge (exchange rate) to our class
                     existingRate.ExchangeRate = conversionRate.ExchangeRate;
                 }
             }
