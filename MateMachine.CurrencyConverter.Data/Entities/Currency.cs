fb2985f23b7e4c6bf28a00d16e85e3e6a98a7f56
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-09 11:23:36 +0430

    Override equals and GetHashCode for Currency class

--- MateMachine.CurrencyConverter.Data/Entities/Currency.cs
+++ MateMachine.CurrencyConverter.Data/Entities/Currency.cs
@@ -5,9 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MateMachine.CurrencyConverter.Data.Entities {
-    public class Currency {
+    public class Currency : IEquatable<Currency> {
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Name { get; set; }
+
+        public bool Equals(Currency? other) {
+            if (other == null) return false;
+            if (this.Name.ToUpper() == other.Name.ToUpper()) return true;
+            return false;
+        }
+
+        public override bool Equals(object obj) {
+            return Equals(obj as Currency);
+        }
+
+        public override int GetHashCode() {
+            int hash = 13;
+            hash = (hash * 7) + Id.GetHashCode();
+            hash = (hash * 7) + FullName.GetHashCode();
+            hash = (hash * 7) + Name.GetHashCode();
+            return hash;
+        }
     }
 }
