8653549ec81ee67b00a75836e2fc921ab6a69160
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-06-09 12:23:22 +0430

    Unit test for currencies with no path

---
+++ MateMachine.CurrencyConverter.Tests/When_No_Route.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateMachine.CurrencyConverter.Tests {
+    [TestClass]
+    public class When_No_Route : ConverterTestBase {
+        [TestMethod]
+        public async Task Then_Result_IsNull() {
+            await Act("USD", "SEK");
+            Assert.IsNull(Result);
+        }
+    }
+}
